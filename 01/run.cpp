99d0fcac36fee7dc310b40318424e993cb9bba45
Dmitriy Klebanov <dmitriy.klebanov@gmail.com>
2019-10-29 23:13:53 +0300

    first commit

---
+++ 01/run.cpp
@@ -0,0 +1,41 @@
+#include "expression.h"
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+int run(std::string&& expr_str) {
+    Expression expr;
+    try {
+        expr.parseFromString(std::move(expr_str));
+    } catch (const std::exception& ex) {
+        std::cout << "Error when parsing expression from string: " << ex.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cout << "Unhandled exception" << std::endl;
+        throw;
+    }
+
+    //expr.printDebug();
+    int res = 0;
+    try {
+        res = expr.evaluate();
+    } catch (const std::exception& ex) {
+        std::cout << "Error when evaluating expression: " << ex.what() << std::endl;
+        return 2;
+    } catch (...) {
+        std::cout << "Unhandled exception" << std::endl;
+        throw;
+    }
+    std::cout << "Result: " << res << std::endl;
+    return 0;
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Invalid arguments. Example:\n";
+        std::cout << "run [expression]" << std::endl;
+        return -1;
+    }
+    return run(std::string(argv[1]));
+}
---
+++ 01/test.cpp
@@ -0,0 +1,43 @@
+#include "expression.h"
+#include "tester.hpp"
+
+#include <functional>
+
+int test() {
+    std::function<bool(const std::string&, const int, const bool)> test_func =
+        [](const std::string& expression, const int ans, const bool thrown_exception) {
+            int res = 0;
+            try {
+                Expression expr;
+                expr.parseFromString(expression);
+                res = expr.evaluate();
+            } catch (...) {
+                return thrown_exception;
+            }
+            return res == ans;
+        };
+    Tester<std::function<bool(const std::string&, const int, const bool)>> expression_tester(test_func);
+
+    expression_tester.execute("", 0, false);
+    expression_tester.execute("-1 +2-3+4 -5+6-7", -4, false);
+    expression_tester.execute("-1+002 - 3*4/5*6/7", 0, false);
+    expression_tester.execute("-1+2-3*4/5* 6 /  7 l", 0, true);
+    expression_tester.execute("-1 + 2/ 3 - 10/5 - -6/2", 0, false);
+    expression_tester.execute("5 + 8* -4/0", 0, true);
+    expression_tester.execute("2+-1-5*2", -9, false);
+    expression_tester.execute("10/2*3+1", 16, false);
+    expression_tester.execute("10+8--1", 19, false);
+    expression_tester.execute("10+8---1", 0, true);
+    expression_tester.execute("10++8--1", 0, true);
+    expression_tester.execute("5+12/-3-5", -4, false);
+    expression_tester.execute("5+", 0, true);
+    expression_tester.execute("+5", 0, true);
+    expression_tester.execute("1/0", 0, true);
+
+    expression_tester.printStatistics();
+    return expression_tester.getTotal() != expression_tester.getSuccess();
+}
+
+int main() {
+    return test();
+}
