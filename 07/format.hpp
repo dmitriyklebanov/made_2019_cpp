9d4ef7b18c0362a3b2ebe94a2125d9980aeead56
Dmitriy Klebanov <dmitriy.klebanov@gmail.com>
2019-12-05 17:19:21 +0300

    removed template copies of format

--- 07/format.hpp
+++ 07/format.hpp
@@ -1,20 +1,9 @@
 #pragma once
 
+#include <limits>
 #include <sstream>
 #include <string>
-
-template <int N>
-void validateFormattedString(const std::string&) {
-}
-
-template <>
-void validateFormattedString<0>(const std::string& str) {
-    if (str.find("{") != std::string::npos ||
-        str.find("}") != std::string::npos)
-    {
-        throw std::runtime_error("format(): error in format string: found unexpected control sign(s) {}");
-    }
-}
+#include <vector>
 
 template <class T>
 std::string toString(const T& val) {
@@ -23,25 +12,69 @@ std::string toString(const T& val) {
     return stream.str();
 }
 
-template <int N = 0, class T>
-std::string format(const std::string& format_str, const T& cur_val) {
-    std::string ans;
-    std::string arg = std::string("{") + toString(N) + "}";
+size_t toSizeT(const std::string& str) {
+    size_t res = 0;
     size_t i = 0;
-    while (format_str.find(arg, i) != std::string::npos) {
-        int j = format_str.find(arg, i);
-        ans += format_str.substr(i, j - i);
-        ans += toString(cur_val);
-        i = j + arg.size();
+    while (i < str.size() && '0' <= str[i] && str[i] <= '9') {
+        size_t max_val = (std::numeric_limits<size_t>::max() - (str[i] - '0')) / 10;
+        if (res > max_val) {
+            throw std::runtime_error("toSizeT(): overflow");
+        }
+        res *= 10;
+        res += str[i] - '0';
+        i++;
     }
-    ans += format_str.substr(i, format_str.size() - i);
-    validateFormattedString<N>(ans);
-    return ans;
+    if (i != str.size()) {
+        throw std::runtime_error("toSizeT(): invalid sign");
+    }
+    return res;
 }
 
 
-template <int N = 0, class T, typename... Args>
-std::string format(const std::string& input, const T& cur_val, Args&&... args) {
-    std::string res = format<N + 1>(input, std::forward<Args>(args)...);
-    return format<N>(res, cur_val);
+template <class T>
+void argsToString(std::vector<std::string>& res, const T& value) {
+    res.push_back(toString(value));
+}
+
+
+template <class T, typename... Args>
+void argsToString(std::vector<std::string>& res, const T& value, Args&&... args) {
+    res.push_back(toString(value));
+    argsToString(res, std::forward<Args>(args)...);
+}
+
+std::string format(const std::string& format_str) {
+    if (format_str.find("{") != std::string::npos ||
+        format_str.find("}") != std::string::npos)
+    {
+        throw std::runtime_error("format(): error in format string: found unexpected control sign(s) {}");
+    }
+    return format_str;
+}
+
+template <typename... Args>
+std::string format(const std::string& format_str, Args&&... args) {
+    std::vector<std::string> strArgs;
+    argsToString(strArgs, std::forward<Args>(args)...);
+
+    std::string ans;
+    size_t i = 0;
+    while (i < format_str.size() && format_str.find("{", i) != std::string::npos) {
+        size_t l = format_str.find("{", i);
+        ans += format_str.substr(i, l - i);
+        if (format_str.find("}", l) != std::string::npos) {
+            size_t r = format_str.find("}", l);
+            size_t id = toSizeT(format_str.substr(l + 1, r - l - 1));
+            if (id >= strArgs.size()) {
+                throw std::runtime_error("format(): argument not found");
+            }
+            ans += strArgs[id];
+            i = r + 1;
+        } else {
+            throw std::runtime_error("format(): error in format string: not found control sign }");
+        }
+    }
+    ans += format_str.substr(i, format_str.size() - i);
+
+    return ans;
 }
